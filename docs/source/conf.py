5b2f60d0f1cbdbf71533dcf4dd287619ae1ba6d3
seqyuan <ahworld07@gmail.com>
2025-12-06 20:33:58 +0800

    readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,15 +14,22 @@ release = '0.2.2'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+# Base extensions
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     'sphinx.ext.intersphinx',
-    'nbsphinx',  # For Jupyter notebook support
     'sphinx.ext.mathjax',
 ]
+
+# Optional extensions
+try:
+    import nbsphinx
+    extensions.append('nbsphinx')  # For Jupyter notebook support
+except ImportError:
+    pass  # nbsphinx not available, skip notebook support
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
@@ -30,8 +37,38 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
+
+# PyData theme options
+html_theme_options = {
+    "github_url": "https://github.com/seqyuan/trackcell",
+    "logo": {
+        "text": "TrackCell",
+    },
+    "use_edit_page_button": True,
+    "show_toc_level": 2,
+    "navbar_end": ["theme-switcher", "navbar-icon-links"],
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/seqyuan/trackcell",
+            "icon": "fa-brands fa-github",
+        },
+        {
+            "name": "PyPI",
+            "url": "https://pypi.org/project/trackcell/",
+            "icon": "fa-brands fa-python",
+        },
+    ],
+}
+
+html_context = {
+    "github_user": "seqyuan",
+    "github_repo": "trackcell",
+    "github_version": "main",
+    "doc_path": "docs/source",
+}
 
 # -- Extension configuration -------------------------------------------------
 
